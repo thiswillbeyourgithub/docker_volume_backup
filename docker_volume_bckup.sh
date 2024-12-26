c32ec611e78c6263758b0d814f3d7d2e475ec565
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-12-26 12:52:19 +0100

    first commit with instructions

---
+++ docker_volume_bckup.sh
@@ -0,0 +1,13 @@
+# zsh shebang
+#
+# define a OUTPUT_DIR var
+#
+# parse zsh arg: container name
+#
+# drfine a func called log that prints messges only if --verbose is passed else nothing
+#
+# for that docker container find the dict of its volume names: path
+#
+# for each of the volumes create a tar backup  compressed into OUTPUT_DIR / volume name / tarfile with date
+#
+# use log func throughout
